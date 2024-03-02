d2aad1c330f2e64e5c6aeaaee19328da8985d510
SrPola <angelcuberoolivares@gmail.com>
2024-03-02 17:23:47 +0100

    Creación del proyecto base

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -2,8 +2,7 @@
     namespace App\Controllers;
 
     class BaseController {
-        public function renderHTML($fileName, $data = []) {
+        public function renderHTML($fileName, $data = []) { // $fileName = String con la ruta del archivo a renderizar, $data = Array con los datos a renderizar
             include($fileName);
         }
     }
-?>
